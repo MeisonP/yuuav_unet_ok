8fccfc59173dab4d18448720fe37eeceede4e839
Mason <pengdengicl@gmail.com>
2018-11-15 16:07:12 +0800

    Initial commit

---
+++ visualization/cmake-build-debug-llvm/CMakeFiles/VS.dir/DependInfo.cmake
@@ -0,0 +1,70 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/mason/Desktop/unet/visualization/visualization.cpp" "/Users/mason/Desktop/unet/visualization/cmake-build-debug-llvm/CMakeFiles/VS.dir/visualization.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/mason/opencv/opencv/build"
+  "/Users/mason/opencv/opencv/include"
+  "/Users/mason/opencv/opencv/include/opencv"
+  "/Users/mason/opencv/opencv/modules/core/include"
+  "/Users/mason/opencv/opencv/modules/flann/include"
+  "/Users/mason/opencv/opencv_contrib/modules/hdf/include"
+  "/Users/mason/opencv/opencv/modules/imgproc/include"
+  "/Users/mason/opencv/opencv/modules/ml/include"
+  "/Users/mason/opencv/opencv/modules/objdetect/include"
+  "/Users/mason/opencv/opencv_contrib/modules/phase_unwrapping/include"
+  "/Users/mason/opencv/opencv/modules/photo/include"
+  "/Users/mason/opencv/opencv_contrib/modules/plot/include"
+  "/Users/mason/opencv/opencv_contrib/modules/reg/include"
+  "/Users/mason/opencv/opencv_contrib/modules/surface_matching/include"
+  "/Users/mason/opencv/opencv/modules/video/include"
+  "/Users/mason/opencv/opencv_contrib/modules/xphoto/include"
+  "/Users/mason/opencv/opencv/modules/dnn/include"
+  "/Users/mason/opencv/opencv_contrib/modules/face/include"
+  "/Users/mason/opencv/opencv_contrib/modules/fuzzy/include"
+  "/Users/mason/opencv/opencv_contrib/modules/hfs/include"
+  "/Users/mason/opencv/opencv_contrib/modules/img_hash/include"
+  "/Users/mason/opencv/opencv/modules/imgcodecs/include"
+  "/Users/mason/opencv/opencv/modules/shape/include"
+  "/Users/mason/opencv/opencv/modules/videoio/include"
+  "/Users/mason/opencv/opencv_contrib/modules/xobjdetect/include"
+  "/Users/mason/opencv/opencv/modules/highgui/include"
+  "/Users/mason/opencv/opencv/modules/superres/include"
+  "/Users/mason/opencv/opencv/modules/ts/include"
+  "/Users/mason/opencv/opencv_contrib/modules/bioinspired/include"
+  "/Users/mason/opencv/opencv_contrib/modules/dnn_objdetect/include"
+  "/Users/mason/opencv/opencv_contrib/modules/dpm/include"
+  "/Users/mason/opencv/opencv/modules/features2d/include"
+  "/Users/mason/opencv/opencv_contrib/modules/line_descriptor/include"
+  "/Users/mason/opencv/opencv_contrib/modules/saliency/include"
+  "/Users/mason/opencv/opencv_contrib/modules/text/include"
+  "/Users/mason/opencv/opencv/modules/calib3d/include"
+  "/Users/mason/opencv/opencv_contrib/modules/ccalib/include"
+  "/Users/mason/opencv/opencv_contrib/modules/datasets/include"
+  "/Users/mason/opencv/opencv_contrib/modules/rgbd/include"
+  "/Users/mason/opencv/opencv_contrib/modules/stereo/include"
+  "/Users/mason/opencv/opencv_contrib/modules/structured_light/include"
+  "/Users/mason/opencv/opencv_contrib/modules/tracking/include"
+  "/Users/mason/opencv/opencv/modules/videostab/include"
+  "/Users/mason/opencv/opencv_contrib/modules/xfeatures2d/include"
+  "/Users/mason/opencv/opencv_contrib/modules/ximgproc/include"
+  "/Users/mason/opencv/opencv_contrib/modules/aruco/include"
+  "/Users/mason/opencv/opencv_contrib/modules/bgsegm/include"
+  "/Users/mason/opencv/opencv_contrib/modules/optflow/include"
+  "/Users/mason/opencv/opencv_contrib/modules/sfm/include"
+  "/Users/mason/opencv/opencv/modules/stitching/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ visualization/cmake-build-debug-llvm/CMakeFiles/VS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VS.dir/visualization.cpp.o"
+  "VS.pdb"
+  "VS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
